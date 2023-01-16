7f486ad9931a6bb2f2c5419ea05e9529125e7022
Pawan Kumar <zxcvbnm@192.168.1.2>
2023-01-17 00:47:37 +0530

    added env variables

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+import env from 'dotenv';
 import express from "express";
 import bodyParser from "body-parser";
 import path from "path";
--- server/src/config/elephantsql.js
+++ server/src/config/elephantsql.js
@@ -1,9 +1,7 @@
 import pg from 'pg';
-//or native libpq bindings
-//var pg = require('pg').native
 
-var conString = "postgres://krbxceqk:MOkgOL35gOjLM883i-OXL2woBvpT8taZ@dumbo.db.elephantsql.com/krbxceqk" //Can be found in the Details page
-var pool = new pg.Client(conString);
+const conString = process.env.POSTGRES_URL;
+const pool = new pg.Client(conString);
 pool.connect(function(err) {
   if(err) {
     return console.error('could not connect to postgres', err);

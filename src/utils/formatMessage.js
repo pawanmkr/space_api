7743a6855d65464788cc74f74a8f153a637e8a32
Simon Paul <samonpaul06@gmail.com>
2023-01-26 16:15:36 +0530

    removed date

--- src/utils/formatMessage.js
+++ src/utils/formatMessage.js
@@ -3,11 +3,6 @@ import dayjs from "dayjs";
 export default function formatMessage(msg) {
     return {
         message: msg.message,
-        author: msg.username,
-        dateAndTime: {
-            date: dayjs().format('DD-MM-YYYY'),
-            day: dayjs().format('dddd'),
-            time: dayjs().format('hh:mm:ss a'),
-        }
+        username: msg.username,
     }
 }
